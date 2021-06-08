02bd113d2e4fbf7d1b9021c59f5efa95fc87caae
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-06-09 00:28:32 +0530

    added videoDB.js

--- src/App.js
+++ src/App.js
@@ -1,68 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
+import { videoDB } from "./videoDB";
 
-var videoDB = {
-  Humour: [
-    {
-      name: "The agony of trying to unsubscribe",
-      speaker: "James Veitch",
-      rating: "4.5/5",
-      link: "https://youtu.be/KUb0JBOIOAA",
-    },
-    {
-      name: "How I hacked online dating",
-      speaker: "Amy Webb",
-      rating: "4/5",
-      link: "https://youtu.be/d6wG_sAdP0U",
-    },
-    {
-      name: "This is what happens when you reply to spam mail",
-      speaker: "James Veitch",
-      rating: "4.5/5",
-      link: "https://youtu.be/LiLS7U7YIdc",
-    },
-  ],
-  "Mental Health": [
-    {
-      name: "The puzzle of motivation",
-      speaker: "Daniel Pink",
-      rating: "4.5/5",
-      link: "https://youtu.be/rrkrvAUbU9Y",
-    },
-    {
-      name: "Inside the mind of a master procrastinator",
-      speaker: "Tim Urban",
-      rating: "5/5",
-      link: "https://youtu.be/arj7oStGLkU",
-    },
-    {
-      name: "What They Don't Tell You About Mental Illness",
-      speaker: "Elizabeth Medina ",
-      rating: "4/5",
-      link: "https://www.youtube.com/watch?v=ieXB-BGxYwg",
-    }
-  ],
-  "Self Improvement": [
-    {
-      name: "How great leaders inspire action?",
-      speaker: "Simon Sinek",
-      rating: "5/5",
-      link: "https://youtu.be/qp0HIF3SfI4",
-    },
-    {
-      name: "How to speak so that people want to listen?",
-      speaker: "Julian Treasure",
-      rating: "4.5/5",
-      link: "https://youtu.be/eIho2S0ZahI",
-    },
-    {
-      name: "Your body language may shape who you are",
-      speaker: "Amy Cuddy",
-      rating: "4/5",
-      link: "https://youtu.be/Ks-_Mh1QhMc",
-    },
-  ],
-};
 
 const videoList = Object.keys(videoDB);
 
---
+++ src/videoDB.js
@@ -0,0 +1,170 @@
+export const videoDB = {
+    Humour: [
+      {
+        name: "The agony of trying to unsubscribe",
+        speaker: "James Veitch",
+        rating: "4.5/5",
+        link: "https://youtu.be/KUb0JBOIOAA",
+      },
+      {
+        name: "How I hacked online dating",
+        speaker: "Amy Webb",
+        rating: "4/5",
+        link: "https://youtu.be/d6wG_sAdP0U",
+      },
+      {
+        name: "This is what happens when you reply to spam mail",
+        speaker: "James Veitch",
+        rating: "4.5/5",
+        link: "https://youtu.be/LiLS7U7YIdc",
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:        
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+    ],
+    "Mental Health": [
+      {
+        name: "The puzzle of motivation",
+        speaker: "Daniel Pink",
+        rating: "4.5/5",
+        link: "https://youtu.be/rrkrvAUbU9Y",
+      },
+      {
+        name: "Inside the mind of a master procrastinator",
+        speaker: "Tim Urban",
+        rating: "5/5",
+        link: "https://youtu.be/arj7oStGLkU",
+      },
+      {
+        name: "What They Don't Tell You About Mental Illness",
+        speaker: "Elizabeth Medina ",
+        rating: "4/5",
+        link: "https://www.youtube.com/watch?v=ieXB-BGxYwg",
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:        
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+    ],
+    "Self Improvement": [
+      {
+        name: "How great leaders inspire action?",
+        speaker: "Simon Sinek",
+        rating: "5/5",
+        link: "https://youtu.be/qp0HIF3SfI4",
+      },
+      {
+        name: "How to speak so that people want to listen?",
+        speaker: "Julian Treasure",
+        rating: "4.5/5",
+        link: "https://youtu.be/eIho2S0ZahI",
+      },
+      {
+        name: "Your body language may shape who you are",
+        speaker: "Amy Cuddy",
+        rating: "4/5",
+        link: "https://youtu.be/Ks-_Mh1QhMc",
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:        
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+      {
+        name:
+        speaker:
+        rating:
+        link:
+      },
+    ],
+  };
